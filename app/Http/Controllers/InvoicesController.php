eeb0585b0d18b75ea53d6f295906c41fcd950416
rogermelich <rogermelich@gmail.com>
2016-01-18 17:58:13 +0100

    Modified Controller to Controller accés, tranform and Push to Database

--- app/Http/Controllers/InvoicesController.php
+++ app/Http/Controllers/InvoicesController.php
@@ -2,18 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Invoices;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class InvoicesController extends Controller
 {
     //
     public function index()
     {
-        
+        if (!Auth::check() ){
+            return "Forbbiden";
+        }
 
-        return view('invoices');
+        $database_invoices = $this->getAllInvoicesFromDatabase();
+
+        $invoices = $this->transform($database_invoices);
+        return view('invoices', compact('invoices'));
+
+        //$data['invoices'] = $invoices;
+        //return view('invoices', $data);
+    }
+
+    private function getAllInvoicesFromDatabase()
+    {
+        return Invoices::all();
+    }
+
+    private function transform($database_invoices)
+    {
+        return $database_invoices;
     }
 }
