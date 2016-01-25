502ef5ea1f7b15195303cdc1e80d977a7e64757c
rogermelich <rogermelich@gmail.com>
2016-01-25 16:45:19 +0100

    Add User Repositiry

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -9,7 +9,47 @@
 namespace app\Repositories;
 
 
-class UserRepository
+use App\User;
+
+class UserRepository implements RepositoryInterface
 {
 
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function all()
+    {
+        return user::all();
+    }
+
+    public function paginate($perPage = 15, $columns = array('*'))
+    {
+        // TODO: Implement paginate() method.
+    }
+
+    public function create(array $data)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function update(array $data, $id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function find($id, $columns = array('*'))
+    {
+        // TODO: Implement find() method.
+    }
+
+    public function findBy($field, $value, $columns = array('*'))
+    {
+        // TODO: Implement findBy() method.
+    }
 }
