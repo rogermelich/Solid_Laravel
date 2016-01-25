1567193575175bb964715d6caa38840952e9e2f1
rogermelich <rogermelich@gmail.com>
2016-01-25 20:24:28 +0100

    Add Flash message de SweetAlert

--- app/Http/Controllers/ContactEmailController.php
+++ app/Http/Controllers/ContactEmailController.php
@@ -9,7 +9,19 @@ use App\Http\Controllers\Controller;
 
 class ContactEmailController extends Controller
 {
-    public function send(){
-        echo "Hola";
+    public function send(Request $request){
+        //dd(Input::all);
+
+        //TODO: SEND EMAIL
+
+        //FLASH NOTIFICATION
+//        $request->session()->flash(
+//            'notification',
+//            'All ok!'
+//        );
+        Flash::message('Ok!');
+
+        //REDIRECT WELCOME
+        return redirect()->route('welcome');
     }
 }
